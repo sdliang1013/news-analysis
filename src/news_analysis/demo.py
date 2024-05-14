24810550814184ff0cfbacafd73554d023d904b7
shidongliang <ylz@123456>
2024-05-14 15:50:13 +0800

    feat: 完善fine tuning代码

---
+++ src/news_analysis/demo.py
@@ -0,0 +1,200 @@
+# 全部流程代码
+
+import numpy as np
+import pandas as pd
+import torch
+from torch import nn
+from torch.optim import Adam
+from torch.utils import data
+from tqdm import tqdm
+from transformers import BertModel, BertTokenizerFast
+
+CACHE_DIR = 'D:/data/huggingface/cache'
+DS_DIR = 'D:/data/huggingface/datasets'
+MODEL_DIR = 'D:/data/huggingface/models'
+
+model_path = f'{MODEL_DIR}/bert-base-chinese'
+token_length = 256
+hidden_size = 768
+
+labels_dict = {
+    "汽车": 1,
+    "财经": 2,
+    "科技": 3,
+    "健康": 4,
+    "体育": 5,
+    "教育": 6,
+    "文化": 7,
+    "军事": 8,
+    "娱乐": 9,
+    "时尚": 0,
+}
+num_labels = len(labels_dict)
+col_label = 'classify'
+col_content = 'content'
+
+
+class Dataset(data.Dataset):
+    def __init__(self, df: pd.DataFrame):
+        # 标签
+        self.labels = [np.int64(labels_dict[label]) for label in df[col_label]]
+        # 文本向量化
+        self.texts = [tokenizer(text,
+                                padding='max_length',
+                                max_length=token_length,
+                                truncation=True,
+                                return_tensors="pt")
+                      for text in df[col_content]]
+
+    def classes(self):
+        return self.labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def get_labels(self, idx):
+        # Fetch a batch of labels
+        return np.array(self.labels[idx])
+
+    def get_texts(self, idx):
+        # Fetch a batch of inputs
+        return self.texts[idx]
+
+    def __getitem__(self, idx):
+        ary_texts = self.get_texts(idx)
+        ary_label = self.get_labels(idx)
+        return ary_texts, ary_label
+
+
+# 构建模型
+class BertClassifier(nn.Module):
+    def __init__(self, dropout=0.5):
+        super(BertClassifier, self).__init__()
+        self.bert = BertModel.from_pretrained(model_path)
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(hidden_size, num_labels)
+        self.relu = nn.ReLU()
+
+    def forward(self, input_id, mask):
+        _, pooled_output = self.bert(input_ids=input_id, attention_mask=mask, return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+        return final_layer
+
+
+# 从上面的代码可以看出，BERT Classifier 模型输出了两个变量：
+# 1. 在上面的代码中命名的第一个变量_包含sequence中所有 token 的 Embedding 向量层。
+# 2. 命名的第二个变量pooled_output包含 [CLS] token 的 Embedding 向量。对于文本分类任务，使用这个 Embedding 作为分类器的输入就足够了。
+# 然后将pooled_output变量传递到具有ReLU激活函数的线性层。在线性层中输出一个维度大小为 5 的向量，每个向量对应于标签类别（运动、商业、政治、 娱乐和科技）。
+
+
+def train(model: nn.Module, train_data: pd.DataFrame, val_data: pd.DataFrame, learning_rate: float, epochs: int):
+    # 通过Dataset类获取训练和验证集
+    ds_train, ds_val = Dataset(train_data), Dataset(val_data)
+    # DataLoader根据batch_size获取数据，训练时选择打乱样本
+    train_loader = data.DataLoader(ds_train, batch_size=100, shuffle=True)
+    val_loader = data.DataLoader(ds_val, batch_size=100)
+    # 判断是否使用GPU
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    # 定义损失函数和优化器
+    criterion = nn.CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr=learning_rate)
+
+    if use_cuda:
+        model = model.cuda()
+        criterion = criterion.cuda()
+    # 开始进入训练循环
+    for epoch_num in range(epochs):
+        # 定义两个变量，用于存储训练集的准确率和损失
+        total_acc_train = 0
+        total_loss_train = 0
+        # 进度条函数tqdm
+        for train_input, train_label in tqdm(train_loader):
+            train_label = train_label.to(device)
+            mask = train_input['attention_mask'].to(device)
+            input_id = train_input['input_ids'].squeeze(1).to(device)
+            # 通过模型得到输出
+            output = model(input_id, mask)
+            # 计算损失
+            batch_loss = criterion(output, train_label)
+            total_loss_train += batch_loss.item()
+            # 计算精度
+            acc = (output.argmax(dim=1) == train_label).sum().item()
+            total_acc_train += acc
+            # 模型更新
+            model.zero_grad()
+            batch_loss.backward()
+            optimizer.step()
+        # ------ 验证模型 -----------
+        # 定义两个变量，用于存储验证集的准确率和损失
+        total_acc_val = 0
+        total_loss_val = 0
+        # 不需要计算梯度
+        with torch.no_grad():
+            # 循环获取数据集，并用训练好的模型进行验证
+            for val_input, val_label in val_loader:
+                # 如果有GPU，则使用GPU，接下来的操作同训练
+                val_label = val_label.to(device)
+                mask = val_input['attention_mask'].to(device)
+                input_id = val_input['input_ids'].squeeze(1).to(device)
+
+                output = model(input_id, mask)
+
+                batch_loss = criterion(output, val_label)
+                total_loss_val += batch_loss.item()
+
+                acc = (output.argmax(dim=1) == val_label).sum().item()
+                total_acc_val += acc
+
+        print(
+            f'''Epochs: {epoch_num + 1} 
+              | Train Loss: {total_loss_train / len(train_data): .3f} 
+              | Train Accuracy: {total_acc_train / len(train_data): .3f} 
+              | Val Loss: {total_loss_val / len(val_data): .3f} 
+              | Val Accuracy: {total_acc_val / len(val_data): .3f}''')
+
+    # 我们对模型进行了 5 个 epoch 的训练，我们使用 Adam 作为优化器，而学习率设置为1e-6。
+
+
+# 测试模型
+def evaluate(model: nn.Module, test_data: pd.DataFrame):
+    ds_test = Dataset(test_data)
+    test_loader = torch.utils.data.DataLoader(ds_test, batch_size=100)
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    if use_cuda:
+        model = model.cuda()
+
+    total_acc_test = 0
+    with torch.no_grad():
+        for test_input, test_label in test_loader:
+            test_label = test_label.to(device)
+            mask = test_input['attention_mask'].to(device)
+            input_id = test_input['input_ids'].squeeze(1).to(device)
+            output = model(input_id, mask)
+            acc = (output.argmax(dim=1) == test_label).sum().item()
+            total_acc_test += acc
+    print(f'Test Accuracy: {total_acc_test / len(test_data): .3f}')
+
+
+if __name__ == '__main__':
+    tokenizer = BertTokenizerFast.from_pretrained(model_path)
+    model = BertClassifier()
+    # 因为本案例中是处理多类分类问题，则使用分类交叉熵作为我们的损失函数。
+    EPOCHS = 5
+    LR = 1e-6
+    # 数据集准备
+    # 拆分训练集、验证集和测试集 8:1:1
+    news_texts = pd.read_csv(f'{DS_DIR}/news/sogou-news.txt.csv')
+    # bbc_text_df.head()
+    news_df = pd.DataFrame(news_texts)
+    np.random.seed(112)
+    df_train, df_val, df_test = np.split(news_df.sample(frac=1, random_state=42),
+                                         [int(0.8 * len(news_df)), int(0.9 * len(news_df))])
+    print(len(df_train), len(df_val), len(df_test))
+    # 训练
+    train(model, df_train, df_val, LR, EPOCHS)
+    # 用测试数据集进行测试
+    evaluate(model, df_test)
--- src/news_analysis/mod_tuning.py
+++ src/news_analysis/mod_tuning.py
@@ -1,32 +1,124 @@
 """test python"""
+from typing import Tuple, MutableMapping
+
 import numpy
 import torch
 from datasets import load_dataset, Dataset
 from transformers import BertTokenizer, PreTrainedTokenizer, PreTrainedModel, BertForSequenceClassification, \
-    BertTokenizerFast, BatchEncoding, TrainingArguments, Trainer
+    BertTokenizerFast, TrainingArguments, Trainer
 
 CACHE_DIR = 'D:/data/huggingface/cache'
 DS_DIR = 'D:/data/huggingface/datasets'
 MODEL_DIR = 'D:/data/huggingface/models'
+token_length = 256
+col_label = 'classify'
+col_content = 'content'
+
+labels_dict = {
+    "汽车": 1,
+    "财经": 2,
+    "科技": 3,
+    "健康": 4,
+    "体育": 5,
+    "教育": 6,
+    "文化": 7,
+    "军事": 8,
+    "娱乐": 9,
+    "时尚": 0,
+}
+num_labels = len(labels_dict)
+
+
+def classify_label(classify: str) -> [int]:
+    # return classify_dict.get(classify, 0)
+    return numpy.array(labels_dict.get(classify, 0))
+
+
+def train_test_split(ds: Dataset, test_ratio: float) -> Tuple[Dataset, Dataset]:
+    ds_split = ds.train_test_split(test_size=test_ratio)
+    return ds_split.get('train'), ds_split.get('test')
+
+
+def conv2input(tokenizer: PreTrainedTokenizer, data: MutableMapping, max_length: int) -> MutableMapping:
+    # 分类
+    labels = list(map(classify_label, data.get(col_label)))
+    # 内容
+    token = tokenizer(text=data.get(col_content), add_special_tokens=True, max_length=max_length,
+                      padding='max_length', truncation=True, return_tensors="pt",
+                      return_token_type_ids=True, return_attention_mask=True, )
+    data.update({
+        "labels": labels,
+        "input_ids": token.get('input_ids'),
+        "attention_mask": token.get('attention_mask'),
+        "token_type_ids": token.get('token_type_ids'),
+    })
+    return data
+
+
+def tuning_news():
+    # load model
+    model_path = f'{MODEL_DIR}/bert-base-chinese'
+    model: PreTrainedModel = BertForSequenceClassification.from_pretrained(model_path,
+                                                                           classifier_dropout=0.5,
+                                                                           num_labels=num_labels)
+    tokenizer = BertTokenizerFast.from_pretrained(model_path)
+    print(model)
+    # load data
+    # df = pd.read_csv(filepath_or_buffer=f'{DS_DIR}/news/sogou-news.txt.csv')
+    ds = load_dataset(path='csv', data_files=[f'{DS_DIR}/news/sogou-news.txt.csv'], cache_dir=CACHE_DIR)
+    # 构建输入向量
+    ds_train, ds_test = train_test_split(ds=ds.get("train"), test_ratio=0.1)
+    ds_train = ds_train.map(function=lambda data: conv2input(tokenizer=tokenizer, data=data, max_length=token_length),
+                            batched=True, batch_size=100)
+    ds_test = ds_test.map(function=lambda data: conv2input(tokenizer=tokenizer, data=data, max_length=token_length),
+                          batched=True, batch_size=100)
+    # 选择列
+    ds_train.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
+    ds_test.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
+
+    print(ds_train)
+    print(ds_test)
+    # 定义优化器,损失函数
+    batch_size = 4
+    epochs = 3
+    warmup_steps = 100
+    weight_decay = 0.01
+    training_args = TrainingArguments(
+        output_dir='./results',
+        num_train_epochs=epochs,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        warmup_steps=warmup_steps,
+        weight_decay=weight_decay,
+        logging_dir='./logs',
+        optim="adamw_torch",  # 修复告警
+    )
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=ds_train,
+        eval_dataset=ds_test,
+    )
+    # 训练
+    trainer.train()
+    # 评估
+    trainer.evaluate()
+    # 保存
+    trainer.save_model(f'{MODEL_DIR}/tuning/')
 
 
 def pad_list(data: list, length: int, pad: any) -> list:
     left = length - len(data)
     if left <= 0:
         return data
-    data = data + [pad for i in range(left)]
     return data + [pad for i in range(left)]
 
 
 def test_bert():
-    model_path = f'{MODEL_DIR}/bert-base-multilingual-uncased'
-    # model_bert = BertModel.from_pretrained(model_path,
-    #                                        output_hidden_states=True,
-    #                                        output_attentions=True)
+    model_path = f'{MODEL_DIR}/bert-base-chinese'
     token_bert = BertTokenizer.from_pretrained(model_path)
     token_bert.add_tokens(new_tokens=['今年', '巴基斯坦', '以色列', '入侵'])
     tokens = token_bert.tokenize('we love you, 我爱你, 今年五月份巴基斯坦被以色列入侵')
-    tokens = ['[CLS]'] + tokens + ['SEP']
     print(tokens)
 
 
@@ -60,87 +152,7 @@ def tokenize(tokenizer: PreTrainedTokenizer, txt: str, ) -> dict:
     }
 
 
-def conv_classify(classify: str) -> int:
-    if classify == '汽车':
-        return 1
-    elif classify == '财经':
-        return 2
-    elif classify == '科技':
-        return 3
-    elif classify == '健康':
-        return 4
-    elif classify == '体育':
-        return 5
-    elif classify == '教育':
-        return 6
-    elif classify == '文化':
-        return 7
-    elif classify == '军事':
-        return 8
-    elif classify == '娱乐':
-        return 9
-    elif classify == '时尚':
-        return 10
-    return 0
-
-
-def train_test_split(ds: Dataset, test_ratio: float) -> (Dataset, Dataset):
-    ds_split = ds.train_test_split(test_size=test_ratio)
-    return ds_split.get('train'), ds_split.get('test')
-
-
-def conv2input(tokenizer: PreTrainedTokenizer, data: dict) -> BatchEncoding:
-    encode = tokenizer.encode_plus(text=data["content"], add_special_tokens=True, max_length=128,
-                                   return_token_type_ids=True, return_attention_mask=True, padding=True,
-                                   truncation=True)
-    return encode
-
-
-def tuning_news():
-    # load model
-    model_path = f'{MODEL_DIR}/bert-base-multilingual-uncased'
-    model: PreTrainedModel = BertForSequenceClassification.from_pretrained(model_path)
-    print(model)
-    tokenizer = BertTokenizerFast.from_pretrained(model_path)
-    # load data
-    # df = pd.read_csv(filepath_or_buffer=f'{DS_DIR}/news/sogou-news.txt.csv')
-    ds = load_dataset(path='csv', data_files=[f'{DS_DIR}/news/sogou-news.txt.csv'], cache_dir=CACHE_DIR)
-    ds_train, ds_test = train_test_split(ds=ds.get("train"), test_ratio=0.1)
-    print(ds_train)
-    print(ds_test)
-    # 构架输入向量
-    data = conv2input(tokenizer=tokenizer, data={"content": "这是一个东西"})
-    # ds_train.set_format('torch', columns=['input_ids', 'attention_mask', 'classify'])
-    # ds_train.set_format('torch', columns=['input_ids', 'attention_mask', 'classify'])
-    # 定义优化器,损失函数
-    batch_size = 8
-    epochs = 2
-    warmup_steps = 500
-    weight_decay = 0.01
-    training_args = TrainingArguments(
-        output_dir='./results',
-        num_train_epochs=epochs,
-        per_device_train_batch_size=batch_size,
-        per_device_eval_batch_size=batch_size,
-        warmup_steps=warmup_steps,
-        weight_decay=weight_decay,
-        logging_dir='./logs',
-        optim="adamw_torch",  # 修复告警
-    )
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=ds_train,
-        eval_dataset=ds_test
-    )
-    # 训练
-    trainer.train()
-    # 评估
-    trainer.evaluate()
-
-
 if __name__ == '__main__':
     # test_bert()
     # np2torch()
     tuning_news()
-    ...
