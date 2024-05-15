86c7bd3bc7a2075f22800d80e3da8233e7b1ef81
shidongliang <ylz@123456>
2024-05-15 18:57:58 +0800

    feat: 调整预模型训练参数

--- src/news_analysis/mod_tuning.py
+++ src/news_analysis/mod_tuning.py
@@ -4,8 +4,8 @@ from typing import Tuple, MutableMapping
 import numpy
 import torch
 from datasets import load_dataset, Dataset
-from transformers import BertTokenizer, PreTrainedTokenizer, PreTrainedModel, BertForSequenceClassification, \
-    BertTokenizerFast, TrainingArguments, Trainer
+from transformers import PreTrainedTokenizer, PreTrainedModel, BertForSequenceClassification, \
+    BertTokenizerFast, TrainingArguments, Trainer, PreTrainedTokenizerBase
 
 BASE_DIR = 'D:/data/huggingface'
 CACHE_DIR = f'{BASE_DIR}/cache'
@@ -44,14 +44,10 @@ def train_test_split(ds: Dataset, test_ratio: float) -> Tuple[Dataset, Dataset]:
 
 def conv2input(tokenizer: PreTrainedTokenizer, data: MutableMapping, max_length: int) -> MutableMapping:
     # 内容
-    token = tokenizer(text=data.get(col_content), add_special_tokens=True, max_length=max_length,
-                      padding='max_length', truncation=True, return_tensors="pt",
-                      return_token_type_ids=True, return_attention_mask=True, )
-    data.update({
-        "input_ids": token.get('input_ids'),
-        "attention_mask": token.get('attention_mask'),
-        "token_type_ids": token.get('token_type_ids'),
-    })
+    tokens = tokenizer(text=data.get(col_content), add_special_tokens=True, max_length=max_length,
+                       padding='max_length', truncation=True, return_tensors="pt",
+                       return_token_type_ids=True, return_attention_mask=True, )
+    data.update(tokens)
     # 分类
     labels = data.get(col_label, [])
     if labels:
@@ -94,13 +90,13 @@ def tuning_news():
     warmup_steps = 100
     weight_decay = 0.01
     training_args = TrainingArguments(
-        output_dir='./results',
+        output_dir=RESULT_DIR,
         num_train_epochs=epochs,
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
         warmup_steps=warmup_steps,
         weight_decay=weight_decay,
-        logging_dir='./logs',
+        logging_dir=LOG_DIR,
         optim="adamw_torch",  # 修复告警
     )
     trainer = Trainer(
@@ -168,11 +164,12 @@ def pad_list(data: list, length: int, pad: any) -> list:
     return data + [pad for i in range(left)]
 
 
-def test_bert():
+def test_tokens():
+    txt = '今年５月，一则“吴宇森罹患喉癌 ”的传闻震动电影圈，虽然老友兼制片人张家振第一 时间辟谣，但因导演本人一直未能现身，还是让不少媒体和影迷心怀疑虑。７月１９日，康复回京的吴宇森首次露面，宣布明年开拍《生死恋》之前会先拍一部西片《独行杀手》（即美版《喋血双雄》），同时对当下中国电影的现状进行了一番点评，最后感叹：“我们还没有真正的华语大片。”＃笼Ｓ械牡佳蔟Ａ分镜头都不会２煞弥校吴宇森澄清了自己的病情并透露新片近况。“在这里要感谢北京的几位医生，是他们发现我颈部有肿瘤，还好是良性，但如果再晚一步的话，就可能恶化了。后来我的治疗因为迁就工作的方便去了美国，经过几个月的治疗和休养，现在身体基本恢复健康了。再次谢谢所有网友和影迷对我的关怀，我现在复原真的很好，我会以后更当心一点。”＝几年，从好莱坞回来的吴宇森耳闻目睹了华语电影圈很多怪现象，一向温和的他也忍不住吐槽，“我听说有些年轻导演很容易得了机会，不知道自己要什么，也不知道镜头摆在哪里。我也听说，有些导演是副导演帮他分镜头。这些情况以前是不会发生的，我们那一代被训练下来，动作戏都是我设计镜头，我跟动作指导在场地中央，两个人拿着枪（是玩具枪），怎么样打怎么样跑怎么跳怎么开枪，我镜头怎么拍出周润发拿枪姿势的美感，就是张彻那样的大导演，也是自己分镜头的，这是一种工作态度，我们到现场就清楚，我的风格是什么！”＃慢Ｎ颐腔姑挥幸徊魁Ｕ嬲的华语大片８粘龅谰透过吴宇森的陈可辛导演曾经说过：“提起Ｊｏｈｎ　Ｗｏｏ（吴宇森英文名），就是拍大片的，这是大家的共识。”正因如此，当听到在好莱坞拍过《变脸》和在内地导过《赤壁》的吴宇森说“我们还没有一部真正的华语大片”时，震撼之余，也多了几分当事人检讨的诚意。！安豢推的讲，我觉得到现在为止我们还没有一部真正的华语大片。其实有一部，可能很多人都不同意的，就是张艺谋的《活着》，有大片的格局，是一个大时代里的故事，有大片那份感觉。现在的大片，以为千军万马都是大片，我的《赤壁》也犯同样的毛病，剧本比较薄弱，太多的篇幅放在场面上，故事缺乏一种浓厚的人文精神，就是功力不足。我不好意思讲太多别人的作品，我只是觉得，到现在我们还没有一部像《七武士》、《阿拉伯的劳伦斯》那样的大片，我们的电影圈没有拍大片的气候，我们的工作团队没有拍大片那种熟练的技术，所以《赤壁》拍得很辛苦。”！按蠹颐挥泄那种经验，在取景，在镜头角度方面，花很多时间去拍摄，不好再来过，换过一个角度再去拍，没有大片细密的设计和计算的精准，制片的筹备功夫还是不够，应该有胡金铨那种画面的工整精确，还有导演创作者团队达不成大片的共识。除了技术，一些演员的演出，比较夸张，表演比较公式化，我怎样看都不像人家的大片来得那么自然，真实的感觉不够。但是大家在努力，年轻人不断加入进来，花更多的钱，更多的时间，让大家学习了解怎么去配合一部大片。当然，这纯粹是个人感觉，我并不处在一个批评的角度，我期待真正的大片出现。我们创作的环境和条件还是不够，制作方面还是有很多改进的空间。”Ｗ魑专业电影人，吴宇森最后表达了和很多业内人士同样的忧虑，至于怎样解决，他没有说，对于一位已经六十六岁的影坛宿将而言，讲再多，不如拍好下一部电影来得实际。'
     model_path = f'{MODEL_DIR}/bert-base-chinese'
-    token_bert = BertTokenizer.from_pretrained(model_path)
-    token_bert.add_tokens(new_tokens=['今年', '巴基斯坦', '以色列', '入侵'])
-    tokens = token_bert.tokenize('we love you, 我爱你, 今年五月份巴基斯坦被以色列入侵')
+    token_bert = BertTokenizerFast.from_pretrained(model_path)
+    token_bert.add_tokens(new_tokens=['今年', '吴宇森', '电影', '导演'])
+    tokens = token_bert.tokenize(text=txt)
     print(tokens)
 
 
@@ -185,29 +182,28 @@ def np2torch():
     print('tensor to array:', tensor2array)
 
 
-def tokenize(tokenizer: PreTrainedTokenizer, txt: str, ) -> dict:
-    length = 50
-    tokens_ognl = ['[CLS]'] + tokenizer.tokenize(text=txt) + ['SEP']
-    len_tokens = len(tokens_ognl)
-    tokens = pad_list(data=tokens_ognl, length=length, pad='[PAD]')
-    # token_ids
-    token_ids = tokenizer.convert_tokens_to_ids(tokens_ognl)
-    token_ids = pad_list(data=token_ids, length=length, pad=0)
-    token_ids = torch.tensor(token_ids).unsqueeze(0)
+def tokenize(tokenizer: PreTrainedTokenizerBase, txt: str) -> dict:
+    tokens = ['[CLS]'] + tokenizer.tokenize(text=txt) + ['SEP']
+    max_length = len(tokens)
+    # tokens = pad_list(data=tokens, length=max_length, pad='[PAD]')
+    # input_ids
+    input_ids = tokenizer.encode(text=txt)
+    input_ids = pad_list(data=input_ids, length=max_length, pad=0)
+    input_ids = torch.tensor(input_ids).unsqueeze(0)
     # attention_mask
-    masks = [1 for i in range(len_tokens)]
-    masks = pad_list(data=masks, length=length, pad=0)
-    seg_ids = [0 for i in range(length)]
+    masks = [1 for i in range(max_length)]
+    masks = pad_list(data=masks, length=max_length, pad=0)
+    token_type_ids = [0 for i in range(max_length)]
     return {
         "tokens": tokens,
-        "token_ids": token_ids,
-        "seg_ids": seg_ids,
+        "input_ids": input_ids,
+        "token_type_ids": token_type_ids,
         "attention_mask": masks,
     }
 
 
 if __name__ == '__main__':
-    # test_bert()
+    # test_tokens()
     # np2torch()
-    tuning_news()
-    # predict_news()
+    # tuning_news()
+    predict_news()
